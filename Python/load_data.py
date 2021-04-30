28e485050b891a0a7d851013580bac1a5a9b6b2e
roelhulsman <roel929@live.nl>
2021-04-30 20:07:52 +0200

    load data

--- Python/load_data.py
+++ Python/load_data.py
@@ -1,26 +1,33 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
 
 # Load data
+data_mzelst = pd.read_csv("Data/mzelst.csv")
 data_lcps_ic = pd.read_csv("Data/lcps_ic_opnames.csv")
 data_nice_ic = pd.read_csv("Data/nice_ic_intake.csv")
 data_rivm_ic = pd.read_csv("Data/rivm_ic_opnames.csv", sep=';')
 
 # Inspect variable types
+data_mzelst.info()
 data_lcps_ic.info()
 data_nice_ic.info()
 data_rivm_ic.info()
 
 # Transform date column from string to datetime
+data_mzelst.date = pd.to_datetime(data_mzelst.date, format='%Y-%m-%d')
 data_lcps_ic.Datum = pd.to_datetime(data_lcps_ic.Datum, format='%d-%m-%Y')
 data_nice_ic.date = pd.to_datetime(data_nice_ic.date, format='%Y-%m-%d')
 data_rivm_ic.Date_of_statistics = pd.to_datetime(data_rivm_ic.Date_of_statistics, format='%Y-%m-%d')
 
 # Sort dataframes to get newest date first
+data_mzelst = data_mzelst.sort_values(by='date', ascending=False)
 data_lcps_ic = data_lcps_ic.sort_values(by='Datum', ascending=False)
 data_nice_ic = data_nice_ic.sort_values(by='date', ascending=False)
 data_rivm_ic = data_rivm_ic.sort_values(by='Date_of_statistics', ascending=False)
 
 # Delete the newest day since it is not complete
+data_mzelst = data_mzelst.drop(427, axis=0)
 data_lcps_ic = data_lcps_ic.drop(0, axis=0)
 data_nice_ic = data_nice_ic.drop(427, axis=0)
 
@@ -30,3 +37,13 @@ data_nice_ic.IC_Intake.head(10) #Aantal nieuwe patiënten met verdachte of bewez
 data_rivm_ic.IC_admission.head(10) #RIVM haalt z'n data van het NICE, waarom het (lichtelijk) afwijkt is mij onbekend
 #De IC admission data van het LCPS lijkt een dag achter te lopen op die van het RIVM/NICE, waarom is mij onbekend
 #De IC admission data van het LCPS is consistent lager dan die van het RIVM/NICE, waarom is mij onbekend
+
+
+plt.scatter(data_mzelst.date, data_mzelst.IC_Intake, s=2, label='IC Admissions')
+plt.scatter(data_mzelst.date, data_mzelst.Hospital_Intake, s=2, label='Hospital Admissions')
+plt.scatter(data_mzelst.date, data_mzelst.positivetests/25, s=2, label='Positive Tests/25')
+plt.axvline(dt.datetime(2020, 3, 12), color='k', lw='1')
+plt.axvline(dt.datetime(2020, 3, 23), color='k', lw='1')
+plt.xlabel('Date')
+plt.legend()
+plt.show()
