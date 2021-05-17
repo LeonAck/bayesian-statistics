a526a6eb662def2000efd26153b14db02f210c83
lback <l.b.ackerman@student.rug.nl/>
2021-05-17 12:18:19 +0200

    Naam bootstrap bestand aangepast naar bootstrap_classes.py. Nieuw betand: perform_bootstrap.py met uitvoering van de bootstrap.

--- Python/perform_bootstrap.py
+++ Python/perform_bootstrap.py
@@ -1,17 +1,14 @@
 """
-Perform a Ridge regression
-Op basis van deze link https://machinelearningmastery.com/ridge-regression-with-python/
+File to perform block_bootstrap
 """
 
 # Import modules
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import RepeatedKFold
-from sklearn.linear_model import Ridge, Lasso, PoissonRegressor, ElasticNet
+from cross_validation import BlockingTimeSeriesSplit
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV
-from cross_validation import BlockingTimeSeriesSplit, GridSearchOwn
+from sklearn.linear_model import Ridge, Lasso, PoissonRegressor, ElasticNet
+from bootstrap_classes import BlockBootstrapStill, BlockBootstrapMoving
 
 # Load data
 master = pd.read_csv("Data/master.csv", index_col=0)
@@ -62,14 +59,7 @@ X = np.delete(data, 0, axis=1)
 # Keep ICU column for y
 y = data[:, 0]
 
-# Split data set into testing and training set. 80% in training set (arbitrary
-# choice)
-X_train = X[:int(X.shape[0]*0.8)]
-X_test = X[int(X.shape[0]*0.8):]
-y_train = y[:int(X.shape[0]*0.8)]
-y_test = y[int(X.shape[0]*0.8):]
-
-#####################  Create model #######################
+####### Define necessary objects for bootstrap functions
 
 # define grid for hyperparameter search
 grid = dict()
@@ -81,45 +71,29 @@ grid['alpha'] = np.arange(0.01, 1, 0.01)
 # Code is found in cross_validation.py
 btscv = BlockingTimeSeriesSplit(n_splits=5)
 
-### Model 1. Programmed GridSearch for hyperparamter my self
 
-######## own
-own_grid = GridSearchOwn(grid=grid['alpha'], cv=btscv, X=X_train, y=y_train,
-                         model=Ridge)
-own_grid.perform_search()
+########### Perform bootstrap
 
-best_lambda = own_grid.best_param
-print(best_lambda)
-# Define Ridge model with hyper parameter
-model = Ridge(alpha=best_lambda)
+# Create class of bootstrap still
+bbstill = BlockBootstrapStill(n_blocks=10)
 
-# Fit the Ridge model on training set
-model.fit(X_train, y_train)
+# Perform the bootstrap with Ridge model
+# You should be able to insert other models than Ridge. But there seems to be
+# some convergence error in Lasso and ElasticNet
+bbstill.perform_bootstrap(X, y, Ridge, grid, btscv, scaler)
 
-# predict y values using the test set
-yhat = model.predict(X_test)
+# print metrics
+print("\nmean squared error: {0} "
+      "\n mean average error: {1} "
+      "\n mean average percentage error: {2}".format(
+    bbstill.metrics[0], bbstill.metrics[1], bbstill.metrics[2]))
 
-# de-standardize the predictions
-yhat = yhat * scaler.scale_[0] + scaler.mean_[0]
+# Perform bootstrap moving.
+# default values of parameters are found in bootstrap_classes.py
+bbmoving=BlockBootstrapMoving()
+bbmoving.perform_bootstrap(X, y, Ridge, grid, btscv, scaler)
 
-# add code here to compare predicted results to y_test
-
-### Model 2 GridSearchCV from scikit.learn ###
-model2 = Ridge()
-
-# define search
-search = GridSearchCV(model2, grid, scoring='neg_mean_absolute_error', cv=btscv,
-                      n_jobs=-1)
-
-# perform the search
-results = search.fit(X_train, y_train)
-
-# print best parameter and score
-print('Best parameter for lambda is: {}'.format(results.best_params_))
-print('Best score for this model: {}'.format(np.absolute(results.best_score_)))
-
-# predict values of y based on y_test
-yhat2 = search.predict(X_test)
-
-print("standardized predictions", yhat2)
-
+print("\nmean squared error: {0} "
+      "\n mean average error: {1} "
+      "\n mean average percentage error: {2}".format(
+    bbmoving.metrics[0], bbmoving.metrics[1], bbmoving.metrics[2]))
