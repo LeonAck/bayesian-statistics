0528960ab82fcf45f1c7ad3aa06d188465f44ccc
lback <l.b.ackerman@student.rug.nl/>
2021-05-22 16:43:44 +0200

    nieuwe grafiekjes

--- Python/graphs.py
+++ Python/graphs.py
@@ -2,13 +2,80 @@ import matplotlib.pyplot as plt
 import datetime as dt
 import pandas as pd
 import numpy as np
+import copy
 from scipy.interpolate import UnivariateSpline
 
+"""
+eerst log dan first difference van regressors"""
 # Load master data
 master = pd.read_csv("Data/master.csv")
+# replace date where vaccin columns are all zero with nan
+master.loc[[21], ['Vacc_Est', 'Vacc_Est_Carehomes', 'Vacc_Adm_GGD',
+            'Vacc_Adm_Hosp', 'Vacc_Adm_Doctors']] = np.NAN
 
+# interpolate nans in RNA data and in vaccin columns
+master = master.interpolate()
+"""
+for column in master.columns:
+    try:
+        master[column] = np.log(master[column])
+        print(master[column])
+    except (ValueError, AttributeError, TypeError):
+        pass
+"""
 # Convert date column to datetime
 master.date = pd.to_datetime(master.date, format='%Y-%m-%d')
+
+# Describe the data
+
+
+# sort data frame
+# master = master.sort_values(by='date', ascending=True)
+# master_no_diff = copy.deepcopy(master)
+# master_no_diff.sort_vlaues(by='date', ascending=True)
+# take first difference of data except for date column
+#for column in master.columns[1:]:
+ #   master[column] = master[column].diff()
+
+# compare cases to ICU inflow
+plt.plot(master.date, master.ICU_Inflow, label='ICU Admissions')
+plt.plot(master.date, master.Cases/100, label='positive cases')
+plt.xlabel('Date')
+plt.ylabel('Cases')
+plt.legend()
+plt.show()
+
+# compare percentage cases to cases
+plt.plot(master.date, np.log(master.Cases), label='Cases')
+plt.plot(master.date, np.log(master.Cases_Pct_SMA7d)*4, label='Percentage of positive cases')
+plt.xlabel('Date')
+plt.ylabel('Cases')
+plt.legend()
+plt.show()
+
+# compare hospital to ICU inflow
+plt.plot(master.date, master.ICU_Inflow_SMA7d*4, label='ICU Admissions')
+plt.plot(master.date, master.Hosp_Inflow_SMA7d, label='Hospital admissions')
+plt.xlabel('Date')
+plt.ylabel('number')
+plt.legend()
+plt.show()
+
+# compare vacc to ICU inflow
+plt.plot(master.date, np.log(master.ICU_Inflow_SMA7d)*4, label='ICU Admissions')
+plt.plot(master.date, np.log(master.Vacc_Est), label='estimated vaccinations')
+plt.xlabel('Date')
+plt.ylabel('number')
+plt.legend()
+plt.show()
+
+# compare RNA to ICU inflow
+plt.plot(master.date, master.ICU_Inflow_SMA7d, label='ICU Admissions')
+plt.plot(master.date, master.RNA_SMA7d/1000000000000, label='RNA')
+plt.xlabel('Date')
+plt.ylabel('number')
+plt.legend()
+plt.show()
 
 # Graph of ICU Intake
 plt.scatter(master.date, master.ICU_Inflow, s=4)
@@ -31,22 +98,6 @@ plt.show()
 
 # Graph of number of COVID cases
 plt.plot(master.date, master.Cases, label='Number of cases')
-plt.xlabel('Date')
-plt.ylabel('Cases')
-plt.legend()
-plt.show()
-
-# Graph of number of COVID cases per age group
-plt.plot(master.date, master.Cases_0_9, label='Agegroup 0-9')
-plt.plot(master.date, master.Cases_10_19, label='Agegroup 10-19')
-plt.plot(master.date, master.Cases_20_29, label='Agegroup 20-29')
-plt.plot(master.date, master.Cases_30_39, label='Agegroup 30-39')
-plt.plot(master.date, master.Cases_40_49, label='Agegroup 40-49')
-plt.plot(master.date, master.Cases_50_59, label='Agegroup 50-59')
-plt.plot(master.date, master.Cases_60_69, label='Agegroup 60-69')
-plt.plot(master.date, master.Cases_70_79, label='Agegroup 70-79')
-plt.plot(master.date, master.Cases_80_89, label='Agegroup 80-89')
-plt.plot(master.date, master.Cases_90_Plus, label='Agegroup 90+')
 plt.xlabel('Date')
 plt.ylabel('Cases')
 plt.legend()
@@ -88,6 +139,15 @@ plt.ylabel('RNA')
 plt.legend()
 plt.show()
 
+# Compare percentage cases to ICU inflow
+plt.plot(master.date, master.ICU_Inflow_SMA7d, label='ICU Admissions')
+plt.plot(master.date, master.Cases_Pct_SMA7d*2.5, label='Percentage of positive cases')
+plt.xlabel('Date')
+plt.ylabel('Cases')
+plt.legend()
+plt.show()
+
+
 # Test to obtain smoothing spline of ICU inflow
 # Not sure if I am doing this right
 x = np.linspace(0, 197, 198)
@@ -97,3 +157,4 @@ xs = np.linspace(0, 197, 198)
 plt.plot(master.date, y, 'ro', ms=5)
 plt.plot(master.date, ss(xs), 'g')
 plt.show()
+
--- Python/models_michiel.py
+++ Python/models_michiel.py
@@ -8,19 +8,19 @@ from sklearn.linear_model import ElasticNet
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import PoissonRegressor
-from cross_validation import BlockingTimeSeriesSplit 
+from cross_validation import BlockingTimeSeriesSplit
 from cross_validation import GridSearchOwn
 
 #### second, we load the data and create a copy dataset
-master = pd.read_csv("Data/master.csv") # load the data
-master2 = master # create a copy dataset
+master = pd.read_csv("Data/master.csv")  # load the data
+master2 = master  # create a copy dataset
 
 #### third, we transform the data
-master = master.set_index("date") # set the date as the index of the data set
-master = master.loc['2021-04-19':'2020-10-30'] # remove data that have zeroes as entries
+master = master.set_index("date")  # set the date as the index of the data set
+master = master.loc['2021-04-19':'2020-10-30']  # remove data that have zeroes as entries
 master.loc[['2021-04-11'], ['Vacc_Est', 'Vacc_Est_Carehomes', 'Vacc_Adm_GGD',
-            'Vacc_Adm_Hosp', 'Vacc_Adm_Doctors']] = np.NAN # replace remaining zeroes by NAN
-master = master.interpolate() # interpolate the missing data
+                            'Vacc_Adm_Hosp', 'Vacc_Adm_Doctors']] = np.NAN  # replace remaining zeroes by NAN
+master = master.interpolate()  # interpolate the missing data
 
 rel_vars = ['ICU_Inflow', 'Hosp_Inflow',
             'Total_Inflow',
@@ -31,27 +31,28 @@ rel_vars = ['ICU_Inflow', 'Hosp_Inflow',
             'Prev_Growth', 'R',
             'RNA',
             'Vacc_Est']
-master = master[rel_vars] # construction of data set that only contains relevant variables
-data = master.values # turn data into ndarray
+master = master[rel_vars]  # construction of data set that only contains relevant variables
+data = master.values  # turn data into ndarray
 
-scaler = StandardScaler() # choose the way of standardisation/normalisation
+scaler = StandardScaler()  # choose the way of standardisation/normalisation
 scaler = scaler.fit(data)
-normalized = scaler.transform(data) # are these two lines the same as data = scaler.fit_transform(data)?
+normalized = scaler.transform(data)  # are these two lines the same as data = scaler.fit_transform(data)?
 
 X = np.delete(data, 0, axis=1)
 y = data[:, 0]
 
 #### fourth, we divide the data into train and test sets, currently 80-20 division
-X_train = X[:int(X.shape[0]*0.8)]
-X_test = X[int(X.shape[0]*0.8):]
-y_train = y[:int(X.shape[0]*0.8)]
-y_test = y[int(X.shape[0]*0.8):]
+X_train = X[:int(X.shape[0] * 0.8)]
+X_test = X[int(X.shape[0] * 0.8):]
+y_train = y[:int(X.shape[0] * 0.8)]
+y_test = y[int(X.shape[0] * 0.8):]
 
 #### fifth, we use the sklearn package to model the data
 # (1) searching the right value of alpha (lambda)
 grid = dict()
-grid['alpha'] = np.arange(0.01, 1, 0.01) # we define the grid for hyperparameter search
-btscv = BlockingTimeSeriesSplit(n_splits=5) # divide training set in accordance with time series structure (code in cross_validation.py)
+grid['alpha'] = np.arange(0.01, 1, 0.01)  # we define the grid for hyperparameter search
+btscv = BlockingTimeSeriesSplit(
+    n_splits=5)  # divide training set in accordance with time series structure (code in cross_validation.py)
 
 # (2) model the elastic net
 own_grid1 = GridSearchOwn(grid=grid['alpha'], cv=btscv, X=X_train, y=y_train, model=ElasticNet)
@@ -59,10 +60,10 @@ own_grid1.perform_search()
 best_lambda1 = own_grid1.best_param
 print(best_lambda1)
 
-regr1 = ElasticNet(alpha=best_lambda1) # elastic net model
-regr1.fit(X_train,y_train) # compute parameters based on train set
-yhat = regr1.predict(X_test) # compute predictions based on test set
-yhat = yhat * scaler.scale_[0] + scaler.mean_[0] # de-standardize results
+regr1 = ElasticNet(alpha=best_lambda1)  # elastic net model
+regr1.fit(X_train, y_train)  # compute parameters based on train set
+yhat = regr1.predict(X_test)  # compute predictions based on test set
+yhat = yhat * scaler.scale_[0] + scaler.mean_[0]  # de-standardize results
 
 # (3) model the lasso
 own_grid2 = GridSearchOwn(grid=grid['alpha'], cv=btscv, X=X_train, y=y_train, model=Lasso)
@@ -70,10 +71,10 @@ own_grid2.perform_search()
 best_lambda2 = own_grid2.best_param
 print(best_lambda2)
 
-regr2 = Lasso(alpha=best_lambda2) # lasso model
-regr2.fit(X_train,y_train) # compute parameters based on train set
-y2hat = regr2.predict(X_test) # compute predictions based on test set
-y2hat = y2hat * scaler.scale_[0] + scaler.mean_[0] # de-standardize results
+regr2 = Lasso(alpha=best_lambda2)  # lasso model
+regr2.fit(X_train, y_train)  # compute parameters based on train set
+y2hat = regr2.predict(X_test)  # compute predictions based on test set
+y2hat = y2hat * scaler.scale_[0] + scaler.mean_[0]  # de-standardize results
 
 # (4) model the ridge
 own_grid3 = GridSearchOwn(grid=grid['alpha'], cv=btscv, X=X_train, y=y_train, model=Ridge)
@@ -81,10 +82,10 @@ own_grid3.perform_search()
 best_lambda3 = own_grid3.best_param
 print(best_lambda3)
 
-regr3 = Ridge(alpha=best_lambda3) # ridge model
-regr3.fit(X_train,y_train) # compute parameters based on test set
-y3hat = regr3.predict(X_test) # compute predictions based on train set
-y3hat = y3hat * scaler.scale_[0] + scaler.mean_[0] # de-standardize results
+regr3 = Ridge(alpha=best_lambda3)  # ridge model
+regr3.fit(X_train, y_train)  # compute parameters based on test set
+y3hat = regr3.predict(X_test)  # compute predictions based on train set
+y3hat = y3hat * scaler.scale_[0] + scaler.mean_[0]  # de-standardize results
 
 # (5) model the poisson regressor (NB poisson regression depends on assumption mean = variance, so can maybe not be used. in lecture it was stressed however that in prediction those assumptions are less important)
 own_grid4 = GridSearchOwn(grid=grid['alpha'], cv=btscv, X=X_train, y=y_train, model=PoissonRegressor)
@@ -92,8 +93,7 @@ own_grid4.perform_search()
 best_lambda4 = own_grid4.best_param
 print(best_lambda4)
 
-regr4 = PoissonRegressor(alpha=best_lambda4) # ridge model
-regr4.fit(X_train,y_train) # compute parameters based on test set
-y4hat = regr4.predict(X_test) # compute predictions based on train set
-y4hat = y4hat * scaler.scale_[0] + scaler.mean_[0] # de-standardize results
-
+regr4 = PoissonRegressor(alpha=best_lambda4)  # ridge model
+regr4.fit(X_train, y_train)  # compute parameters based on test set
+y4hat = regr4.predict(X_test)  # compute predictions based on train set
+y4hat = y4hat * scaler.scale_[0] + scaler.mean_[0]  # de-standardize results
